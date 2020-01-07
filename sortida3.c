9ce45fa5072c9e8a095a7653d0b2f19dbffb2e50
ireneve0712 <ireneveguillas2005@gmail.com>
2020-01-07 12:41:38 +0100

    Bug fixed in Main and Instructions for Missions

---
+++ sortida3.c
@@ -0,0 +1,11 @@
+#pragma config(Sensor, S3,     ,               sensorEV3_Color)
+#pragma config(Sensor, S4,     ,               sensorEV3_Color)
+// Nom sortida; Missió a fer: M??, M??; Gadget; Comentari
+
+#include "Core/Core.h"
+
+task main()
+{
+Recta(1, 2, 70); // vaig cap a no sé que és una prova
+
+}
