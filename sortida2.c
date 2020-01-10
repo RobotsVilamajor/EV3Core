bac4ad02bf3b7d9b7e1b97b49c749dc4b3f73b85
ireneve0712 <ireneveguillas2005@gmail.com>
2020-01-10 20:16:06 +0100

    Sortida 2: Motors defined and errors corrected

--- sortida2.c
+++ sortida2.c
@@ -1,7 +1,7 @@
 #pragma config(Sensor, S3,     ,               sensorEV3_Color)
 #pragma config(Sensor, S4,     ,               sensorEV3_Color)
 //Sortida Grua; Missions: M2, M12; Gadget 2
-// MotorA = . MotorB =
+// MotorA = Vertical. MotorD = Horitzontal.
 
 #include "Core/Core.h"
 
@@ -10,16 +10,16 @@ task main()
 	//Avancem i deixem pessa blanca (M12)
 	Recta(1, 4, 70);
 
-	//Tirem enrrera i apartem pala
+	//Tirem enrrere i apartem pala
 	Recta(-1, 2, 70);
 	setMotorTarget(motorA, 1, 20);
 
-	//Avancem i movem el cup petit fins el cercle blau (M2: Part 1)
+	//Avancem i movem el cub petit fins el cercle blau (M2: Part 1)
 	Recta(1, 1, 70);
 	setMotorTarget(motorA,-0.5, 20);
 	Recta(1, 3, 70);
 
-	//Tirem enrrera i baixem grua (M2: Part 2)
+	//Tirem enrrere i baixem grua (M2: Part 2)
 	Recta(-1, 2, 70);
 	setMotorTarget(motorA,0.2, 20);
 
