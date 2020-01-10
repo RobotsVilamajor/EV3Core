da2c4f20f30491b3b25aeb93df319be54b134a9e
ireneve0712 <ireneveguillas2005@gmail.com>
2020-01-10 20:35:03 +0100

    Sortida 4: Missions and motors defined

--- sortida4.c
+++ sortida4.c
@@ -1,5 +1,7 @@
 #pragma config(Sensor, S3,     ,               sensorEV3_Color)
 #pragma config(Sensor, S4,     ,               sensorEV3_Color)
+//Sortia rampa; Missió a fer: M1, M11 i M13.
+//MotorA = Vertical. MotorD = Horitzontal.
 
 #include "Core/Core.h"
 
