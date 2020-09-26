f1bd4f575cfc5ee77f7e95d2cc38c183aa368462
JanSolbes <jansolbesgodina@gmail.com>
2020-09-26 13:00:53 +0200

    Plantilla PID

---
+++ Core/src/PID.h
@@ -0,0 +1,32 @@
+#ifndef _PID
+#define _PID
+
+
+struct PID
+{
+	float kp, ki, kd;
+	float integ;
+	float prevError;
+	float dt;
+}
+
+void InitPID(PID& pid, float kp, float ki, float kd, float dt);
+{
+	pid.kp = kp;
+	pid.ki = ki;
+	pid.kd = kd;
+	pid.dt = dt;
+	pid.integ = 0;
+	pid.prevError = 0;
+}
+
+float UpdatePID(PID pid, float error)
+{
+	pid.integ += error * pid.dt;
+	float output = pid.kp * error + pid.ki * pid.integ * pid.dt + pid.kd * (error - pid.prevError) / pid.dt;
+	pid.prevError = error;
+	delay (pid.dt * 1000);
+	return output;
+}
+
+#endif
