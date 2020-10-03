de1840aef7f8de18f53cb01d1a4584780a715029
ireneve0712 <ireneveguillas2005@gmail.com>
2020-10-03 20:24:31 +0200

    function "Recta" created

---
+++ Core/src/Recta.h
@@ -0,0 +1,38 @@
+#ifndef _RECTA
+#define _RECTA
+
+#include "PID.h"
+#include "Accelerate.h"
+
+void Recta (float dir, float distance, float speed)
+{
+	resetMotorEncoder(motorB);
+	resetMotorEncoder(motorC);
+
+	PID pid;
+	InitPID(PID, 300, 200, 1, 0.005);
+
+	Accelerate acc;
+	InitAcc (acc, speed, distance, 7, 5, 5, 5, 0.1);
+
+	while (fabs(getMotorEncoder(motorC) / 360.0) <= distance)
+	{
+		float x = fabs(getMotorEncoder(motorC) / 360.0);
+		float c_speed = GetSpeed(acc, x);
+
+		float error = (getMotorEncoder(motorB) - getMotorEncoder(motorC)) * (360.0 * dir);
+
+		float turnRate = UpdatePID(pid, error);
+
+		setMotorSpeed(motorB, (c_speed + turnRate) * dir);
+		setMotorSpeed(motorC, (c_speed - turnRate) * dir);
+	}
+
+	setMotorSpeed(motorB, 0);
+  setMotorSpeed(motorC, 0);
+
+	resetMotorEncoder(motorB);
+	resetMotorEncoder(motorC);
+}
+
+#endif
