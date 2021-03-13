e5dc86b3cea000885ed9f3fe06c9940fa981208f
nullptr <eliesbertran@gmail.com>
2021-03-13 17:19:42 +0100

    WIP: initial commit

--- Core/src/Turn.h
+++ Core/src/Turn.h
@@ -4,47 +4,48 @@
 #include "Functions.h"
 #include "Defines.h"
 #include "Accelerate.h"
+void AdjustAngle(float angle)
+{
+	float error = getGyroDegrees(S2) - angle;
 
+		writeDebugStreamLine("error: %f", error);
+
+		//if (!InBetween(error, -0.005, 0.005))
+		if (error != 0)
+		{
+			float dir = 0;
+
+			if (error < 0)
+				dir = -1;
+			else
+				dir = 1;
+
+			setMotorSync(motorB, motorC, 100 * dir, 7);
+
+			waitUntil((getGyroDegrees(S2) - angle) * dir <= 0);
+
+			setMotorSpeed(motorB, 0);
+  		setMotorSpeed(motorC, 0);
+
+			//delay(100);
+		}
+}
 
 void Turn(float dir, float angle, float speed)
 {
-	resetMotorEncoder(motorB);
-	resetMotorEncoder(motorC);
-
-	float target = angle / TURN_CONVERSION;
-
 	Accelerate acc;
-	InitAcc (acc, speed, target, 2000, 2000, 2, 2, 7, 7, 0.1);
+	InitAcc(acc, speed, target, 1000, 2000, 2, 2, 7, 7, 0.1, false, true);
 
-	while (fabs(getMotorEncoder(motorB)) / 360.0 < target || fabs(getMotorEncoder(motorC)) / 360.0 < target)
+	setMotorSync(motorB, motorC, 100 * dir, 30);
+	waitUntil(getGyroDegrees(S2) >= angle);
+
+
+	//delay(1000);
+
+	for (int i = 0; i < 100; i++)
 	{
-		float c_speed = GetSpeed(acc, fabs(getMotorEncoder(motorB)) / 360.0);
-
-		setMotorSpeed(motorB, c_speed * dir);
-		setMotorSpeed(motorC, -c_speed * dir);
-
-		//datalogAddValue(0, (int)(c_speed));
-
-		delay(10);
+		AdjustAngle(angle * dir);
 	}
-
-	float time = 1;
-
-	clearTimer(T1);
-	while (time1[T1] < time * 1000)
-	{
-		AdjustWheel (motorB, target * dir);
-		AdjustWheel (motorC, -target * dir);
-	}
-
-	displayCenteredBigTextLine(2, "%.3f - %.3f", getMotorEncoder(motorB), (TURN_CONVERSION * getMotorEncoder(motorB)) / 360.0);
-	displayCenteredBigTextLine(5, "%.3f - %.3f", getMotorEncoder(motorC), (TURN_CONVERSION * getMotorEncoder(motorC)) / 360.0);
-
-	setMotorSpeed(motorB, 0);
-	setMotorSpeed(motorC, 0);
-
-	resetMotorEncoder(motorB);
-	resetMotorEncoder(motorC);
 }
 
 #endif
