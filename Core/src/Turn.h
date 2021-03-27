ef1d87435eb40d713d4c4f7cc213400092044c58
Xavier Torret - BGEO <xtorret@bgeo.es>
2021-03-27 18:49:58 +0100

    Fixed Turn function

--- Core/src/Turn.h
+++ Core/src/Turn.h
@@ -10,44 +10,47 @@ void AdjustAngle(float angle)
 {
 	float error = getGyroDegrees(S2) - angle;
 
-		writeDebugStreamLine("error: %f", error);
+	//writeDebugStreamLine("error: %f", error);
 
-		//if (!InBetween(error, -0.005, 0.005))
-		if (error != 0)
-		{
-			float dir = 0;
+	//if (!InBetween(error, -0.005, 0.005))
+	if (error != 0)
+	{
+		float dir = 0;
 
-			if (error < 0)
-				dir = -1;
-			else
-				dir = 1;
+		if (error < 0)
+			dir = -1;
+		else
+			dir = 1;
 
-			setMotorSync(motorB, motorC, 100 * dir, 7);
+		setMotorSync(motorB, motorC, 100 * dir, 7);
 
-			waitUntil((getGyroDegrees(S2) - angle) * dir <= 0);
+		waitUntil((getGyroDegrees(S2) - angle) * dir <= 0);
 
-			setMotorSpeed(motorB, 0);
-  		setMotorSpeed(motorC, 0);
+		setMotorSpeed(motorB, 0);
+		setMotorSpeed(motorC, 0);
 
-			//delay(100);
-		}
+		delay(100);
+	}
 }
 
 void Turn(float dir, float angle, float speed)
 {
-	Accelerate acc;
-	InitAcc(acc, speed, angle, 1000, 2000, 2, 2, 7, 7, 0.1, false, true);
+	resetGyro(S2);
 
-	while (fabs(getGyroDegrees(S2)) >= angle)
+	Accelerate acc;
+	InitAcc(acc, speed, angle, 1, 0.04, 2, 2, 7, 7, 5, false, true);
+
+	while (fabs(getGyroDegrees(S2)) <= angle)
 	{
-		float c_speed = GetAcc(acc, angle);
+		float c_speed = GetAcc(acc, fabs(getGyroDegrees(S2)));
+		writeDebugStreamLine("%f: %f", fabs(getGyroDegrees(S2)), c_speed);
 
 		setMotorSync(motorB, motorC, 100 * dir, c_speed);
 	}
 
 	for (int i = 0; i < 100; i++)
 	{
-		AdjustAngle(angle * dir);
+		AdjustAngle(-angle * dir);
 	}
 
 	setMotorSpeed(motorB, 0);
