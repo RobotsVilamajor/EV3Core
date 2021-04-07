3adff268bb89a94ffffbc3498d94828b8c96c10a
Xavier Torret - BGEO <xtorret@bgeo.es>
2021-04-07 19:16:22 +0200

    S4 improved

--- Sortida_4.c
+++ Sortida_4.c
@@ -19,46 +19,53 @@ task main()
 {
 	//Fem M??
 	//Align
-	MoveMotorTime(motorA, 3, -70)
-	MoveMotorTime(motorD, 4, 70)
-	MoveMotorTime(motorA, 0.05, 70)
-	MoveMotor(motorD, -6.2, 50);
-	resetGyro(S2);
-	waitForButtonPress();
+	//MoveMotorTime(motorA, 3, -70)
+	//MoveMotorTime(motorD, 4, 70)
+	//MoveMotorTime(motorA, 0.05, 70)
+	//MoveMotor(motorD, -6.2, 50);
+	//resetGyro(S2);
+	//waitForButtonPress();
 
 
 	//Fer ajust inicial
-	delay(250);
-	Recta(Bwd, 0.08, 20);
-	delay(250);
+	//delay(250);
+	//Recta(Bwd, 0.08, 20);
+	//delay(250);
 
 	//Fem M2
-	Recta(Fwd, 6.5, 50);
+	//Recta(Fwd, 6.5, 50);
+	//setMotorSpeed(motorB, 50);
+	//setMotorSpeed(motorC, 50);
+	//delay(250);
+	//setMotorSpeed(motorB, 0);
+	//setMotorSpeed(motorC, 0);
 
-	Recta(Bwd, 1, 30);
-	MoveMotor(motorA, 1.1, 30);
-	delay(10);
-	Recta(Fwd, 0.8, 30);
+
+	//Recta(Bwd, 1, 30);
+	//MoveMotor(motorD, 2.2, 70);
+	//delay(10);
+	//Recta(Fwd, 0.8, 30);
+	//delay(10);
 	Turn(Lft, 90, 50);
+	//MoveMotor(motorA, 1.1, 70);
+
+	////Ens col·loquem a la paret
+	//Recta(Bwd, 2, 50);
+
+	//////Avançem fins a la M14
+	//Recta(Fwd, 1, 50);
 
 
-	//Ens col·loquem a la paret
-	Recta(Bwd, 2, 50);
+	//////Fem la M14
+	//MoveMotorAsync(motorA, 0.5, 20);
+	//MoveMotor(motorD, 0.8, 70);
+	//Recta(Fwd, 0.5, 20);
+	//MoveMotor(motorD, -1, 20);
+	//Recta(Bwd, 0.5, 20);
+	//MoveMotorAsync(motorA, 1, 20);
+	//MoveMotor(motorD, -2.5, 20);
 
-	////Avançem fins a la M14
-	Recta(Fwd, 1, 50);
-
-
-	////Fem la M14
-	MoveMotorAsync(motorA, 0.5, 20);
-	MoveMotor(motorD, 3, 20);
-	Recta(Fwd, 0.3, 20);
-	MoveMotor(motorD, -0.5, 20);
-	Recta(Bwd, 0.3, 20);
-	MoveMotorAsync(motorA, -0.5, 20);
-	MoveMotor(motorD, -2.5, 20);
-
-	////Avançem fins a M8
+	//////Avançem fins a M8
 	//Recta(Fwd, 2.5, 50);
 
 	////Fem M8
