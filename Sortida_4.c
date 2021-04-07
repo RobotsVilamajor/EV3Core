d5984588a7036ba2f5246769f75b259c21473610
JanSolbes <jansolbesgodina@gmail.com>
2021-04-07 22:04:29 +0200

    Mission 4 almost finished

--- Sortida_4.c
+++ Sortida_4.c
@@ -19,12 +19,12 @@ task main()
 {
 	//Fem M??
 	//Align
-	//MoveMotorTime(motorA, 3, -70)
-	//MoveMotorTime(motorD, 4, 70)
-	//MoveMotorTime(motorA, 0.05, 70)
-	//MoveMotor(motorD, -6.2, 50);
-	//resetGyro(S2);
-	//waitForButtonPress();
+	MoveMotorTime(motorA, 3, -70)
+	MoveMotorTime(motorD, 4, 70)
+	MoveMotorTime(motorA, 0.7, 70)
+	MoveMotor(motorD, -6.2, 50);
+	resetGyro(S2);
+	waitForButtonPress();
 
 
 	//Fer ajust inicial
@@ -33,40 +33,46 @@ task main()
 	//delay(250);
 
 	//Fem M2
-	//Recta(Fwd, 6.5, 50);
-	//setMotorSpeed(motorB, 50);
-	//setMotorSpeed(motorC, 50);
-	//delay(250);
-	//setMotorSpeed(motorB, 0);
-	//setMotorSpeed(motorC, 0);
+	Recta(Fwd, 6.4, 50);
+	setMotorSpeed(motorB, 50);
+	setMotorSpeed(motorC, 50);
+	delay(200);
+	setMotorSpeed(motorB, 0);
+	setMotorSpeed(motorC, 0);
+
+	//desenganchem
+	Recta(Bwd, 0.8, 30);
+	MoveMotor(motorA, 0.15, 70);
+	MoveMotor(motorD, 0.1, 70);
 
 
-	//Recta(Bwd, 1, 30);
-	//MoveMotor(motorD, 2.2, 70);
-	//delay(10);
-	//Recta(Fwd, 0.8, 30);
-	//delay(10);
+	//gir
+	Recta(Fwd, 0.9, 30);
+	Recta(Bwd, 0.27, 30);
 	Turn(Lft, 90, 50);
-	//MoveMotor(motorA, 1.1, 70);
 
-	////Ens col·loquem a la paret
-	//Recta(Bwd, 2, 50);
-
-	//////Avançem fins a la M14
-	//Recta(Fwd, 1, 50);
+	//Ens col·loquem a la paret
+	Recta(Bwd, 1, 50);
+	resetGyro(S2);
 
 
-	//////Fem la M14
-	//MoveMotorAsync(motorA, 0.5, 20);
-	//MoveMotor(motorD, 0.8, 70);
-	//Recta(Fwd, 0.5, 20);
-	//MoveMotor(motorD, -1, 20);
-	//Recta(Bwd, 0.5, 20);
-	//MoveMotorAsync(motorA, 1, 20);
-	//MoveMotor(motorD, -2.5, 20);
 
-	//////Avançem fins a M8
-	//Recta(Fwd, 2.5, 50);
+	//Avançem fins a la M14
+	MoveMotor(motorA, 0.8, 70);
+	MoveMotorAsync(motorD, 3, 70);
+	Recta(Fwd, 1.5, 50);
+
+
+	//Fem la M14
+	MoveMotor(motorD, 1, -30);
+	Recta(Bwd, 0.4, 40);
+	MoveMotor(motorA, 0.8, -50);
+	MoveMotor(motorD, 2.25, -70);
+
+	//Avançem fins a M8
+	Recta(Fwd, 3, 50);
+	Recta(Fwd, 0.5, 30);
+
 
 	////Fem M8
 	//Recta(Fwd, 1, 20);
