f8653a7bdcc1dfe496eb28d1bfae19eb60559981
nullptr <eliesbertran@gmail.com>
2021-05-23 22:15:52 +0200

    Updated WaitForLine and "finished" FollowLine

--- Core/src/FollowLine.h
+++ Core/src/FollowLine.h
@@ -1,7 +1,7 @@
 #include "PID.h"
 #include "Defines.h"
 
-void FollowLine(int dir, float distance, float speed, bool bAcc = true, bool bDec = true)
+void FollowLine(float distance, float speed, float sensor, float side, bool bAcc = true, bool bDec = true)
 {
 	resetMotorEncoder(motorB);
 	resetMotorEncoder(motorC);
@@ -13,19 +13,23 @@ void FollowLine(int dir, float distance, float speed, bool bAcc = true, bool bDe
 	}
 
 	PID pid;
-	InitPID(pid, 300, 200, 1, 0.005);
+	InitPID(pid, 0.5, 0.0, 0.02, 0.005);
 
 	Accelerate acc;
 	InitAcc(acc, speed, distance, 1000, 800, 2, 2, 7, 7, 0.1, bAcc, bDec);
 
 	while (fabs(getMotorEncoder(motorC) / 360.0) <= distance || fabs(getMotorEncoder(motorB) / 360.0) <= distance)
 	{
-		float error = getColorReflected(S1) - 50;
+		float error = 50 - getColorReflected(sensor == Lft ? S4 : S3);
 
-		float turnRate = UpdatePID(pid, error);
+		float turnRate = UpdatePID(pid, error) * side;
 
 		float x = fabs(getMotorEncoder(motorC) / 360.0);
-		float c_speed = GetAcc(acc, x);
+		float c_speed = -GetAcc(acc, x);
+
+		datalogAddValue(0, error);
+		datalogAddValue(1, turnRate);
+		datalogAddValue(2, pid.ki * pid.integ * pid.dt);
 
 		setMotorSync(motorB, motorC, turnRate, c_speed);
 	}
--- Core/src/WaitForLine.h
+++ Core/src/WaitForLine.h
@@ -2,9 +2,6 @@
 
 void WaitForLine(float dir, float distance, float speed, float side)
 {
-	resetMotorEncoder(motorB);
-	resetMotorEncoder(motorC);
-
 	setMotorSync(motorB, motorC, 0, speed * dir);
 
 	waitUntil(getColorReflected(side == Lft ? S4 : S3) > LINE_WHITE_MIN || fabs(getMotorEncoder(motorB) / 360.0) >= distance);
